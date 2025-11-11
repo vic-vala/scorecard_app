913dc0993117a3fa91881788d798ebfc7b01da65
vic-vala <vevalade@asu.edu>
2025-11-11 09:54:10 -0700

    feat/refactor: created the `Application` class & organized tasks into functions
    Having the `Application` class makes `main` way
    more readable & maintainable. We can even have
    several instances & pass data between them. This
    is a good framework to continue building our `main`
    as it gets more complex.

--- main.py
+++ main.py
@@ -13,6 +13,101 @@ from src import (
     select_rows_gui,
 )
 
+class Application:
+
+    def __init__(self, config):
+        """
+        Initialize fields reused through the application lifetime
+        """
+        self.config = config
+        self.paths = config['paths']
+        self.sc_settings = config['scorecard_gen_settings']
+        self.data_vis_settings = config['data_vis_settings']
+        self.include_llm_insights = str(self.sc_settings.get("include_LLM_insights", "false")).lower() == "true"
+        self.overwrite_json = str(config.get("overwrite_settings", {}).get("overwrite_json", "false")).lower() == "true"
+
+        # Set later
+        self.overwrite_csv = None
+        self.csv_path = None
+        self.viable_scorecards_tuple = None
+        self.viable_scorecards = None
+        self.scorecards_to_generate = None
+        self.selected_scorecard_courses = None
+        self.selected_history_courses = None
+        self.selected_scorecard_instructors = None
+        print("✅ Config file loaded")
+
+    def parse_excel(self):
+        print("📊 Starting Excel parser")
+        self.overwrite_csv = str(self.config.get("overwrite_settings", {}).get("overwrite_csv", "false")).lower() == "true"
+        self.csv_path = excel_parser.run_excel_parser(self.paths['excel_source'], output_dir=self.paths['csv_dir'], overwrite_csv=self.overwrite_csv)
+
+        # Clean CSV data
+        if (self.overwrite_csv):
+            csv_cleaner.clean_csv(self.csv_path[0])
+
+    def parse_pdfs(self):
+        print("📄 Starting PDF parser")
+        pdf_parser.run_pdf_parser(self.paths['pdf_source'], self.paths['parsed_pdf_dir'], overwrite_json=self.overwrite_json)
+
+    def find_viable_scorecards(self):
+        print("🔗 Finding viable courses for scorecard creation")
+        self.viable_scorecards_tuple = data_handler.viable_scorecards(self.paths['parsed_pdf_dir'], self.csv_path[0])
+        self.viable_scorecards = self.viable_scorecards_tuple[0]
+        self.scorecards_to_generate = self.viable_scorecards_tuple[1]
+
+    def scorecard_selection(self):
+        print("🖥️ Opening Scorecard Selection GUI")
+        self.selected_scorecard_courses = select_rows_gui.select_rows_gui(self.viable_scorecards)
+        print(f"  ✅ {len(self.selected_scorecard_courses)} course(s) selected.")
+
+    def history_selection(self):
+        unique_courses = data_handler.get_unique_courses(self.csv_path[0])
+        print("🖥️ Opening Course History Selection GUI")
+        self.selected_history_courses = select_rows_gui.select_rows_gui(unique_courses)
+        print(f"  ✅ {len(self.selected_history_courses)} course(s) selected for history graphs.")
+
+    def instructor_selection(self):
+        instructors = data_handler.get_instructors(self.csv_path[0])
+        print("🖥️ Opening Instructor Selection GUI")
+        self.selected_scorecard_instructors = select_rows_gui.select_rows_gui(instructors)
+        print(f"  ✅ {len(self.selected_scorecard_instructors)} instructor(s) selected.")
+    
+    def gather_llm_insights(self):
+        """
+        Populate LLM insight fields for viable scorecards.
+        A viable scorecard has a matching `csv_row` and existing
+        `./path/to/parsed_pdf.json`. 
+        """
+
+        if (self.include_llm_insights):
+            print("🤖 Running LLM I/O")   
+            llm_io.run_llm(
+                    gguf_path=self.paths['gguf_path'],
+                    scorecards_to_generate= self.scorecards_to_generate,
+                    llm_dir= self.paths['llm_prompt_dir'],
+                    temp_dir= self.paths['temp_dir'])
+    
+    def generate_data_visualizations(self):
+        print("📈 Generating Data Visualizations")
+        data_vis.generate_data_visualization(
+            self.config, 
+            self.selected_scorecard_courses, 
+            self.selected_scorecard_instructors, 
+            self.csv_path[0], 
+            self.selected_history_courses)
+        
+    def create_scorecards(self):
+        # List of this tuple: (csv row, matching json pdf)
+        print("📝 Generating LaTeX")
+        # Iterate through the scorecards to generate one at a time
+        for scorecard_set in self.scorecards_to_generate:
+            scorecard_assembler.assemble_scorecard(
+                scorecard_set=scorecard_set, 
+                histogram_dir=self.paths['grade_histogram_dir'],
+                tex_output_path=self.paths['tex_dir'],
+                scorecard_output_path=self.paths['scorecard_dir'])
+
 if __name__ == "__main__":
     try:
         # Config GUI
@@ -20,82 +115,22 @@ if __name__ == "__main__":
         print("🖥️ Opening Config GUI")
         config_gui.open_config_editor(CONFIG_PATH)
 
-        # Load config file
-        config = utils.load_config()
-        paths = config['paths']
-        comparison = config["comparison"]
-        scorecard_settings = config['scorecard_gen_settings']
-        data_vis_settings = config['data_vis_settings']
-        include_llm_insights = str(scorecard_settings.get("include_LLM_insights", "false")).lower() == "true"
-        overwrite_json = str(config.get("overwrite_settings", {}).get("overwrite_json", "false")).lower() == "true"
-        print("✅ Config file loaded")
-
-        utils.verify_directories(config['paths'])
-
-        # Parse Excel
-        print("📊 Starting Excel parser")
-        overwrite_csv = str(config.get("overwrite_settings", {}).get("overwrite_csv", "false")).lower() == "true"
-        csv_path = excel_parser.run_excel_parser(paths['excel_source'], output_dir=paths['csv_dir'], overwrite_csv=overwrite_csv)
-
-        # Clean CSV data
-        if (overwrite_csv):
-            csv_cleaner.clean_csv(csv_path[0])
-
-        # Parse PDFs
-        print("📄 Starting PDF parser")
-        pdf_parser.run_pdf_parser(paths['pdf_source'], paths['parsed_pdf_dir'], overwrite_json=overwrite_json)
+        # Application object to logically organize tasks
+        app = Application(config=utils.load_config())
+        
+        utils.verify_directories(app.paths)
+        app.parse_excel()
+        app.parse_pdfs()
 
         # Use CSV/PDF overlap to find viable scorecards
-        print("🔗 Finding viable courses for scorecard creation")
-        viable_scorecards_tuple = data_handler.viable_scorecards(paths['parsed_pdf_dir'],csv_path[0])
+        app.find_viable_scorecards()
 
-        viable_scorecards = viable_scorecards_tuple[0]
-        # Course scorecard selection GUI
-        print("🖥️ Opening Scorecard Selection GUI")
-        selected_scorecard_courses = select_rows_gui.select_rows_gui(viable_scorecards)
-        print(f"  ✅ {len(selected_scorecard_courses)} course(s) selected.")
-
-        # Course history selection GUI (unique courses)
-        unique_courses = data_handler.get_unique_courses(csv_path[0])
-        print("🖥️ Opening Course History Selection GUI")
-        selected_history_courses = select_rows_gui.select_rows_gui(unique_courses)
-        print(f"  ✅ {len(selected_history_courses)} course(s) selected for history graphs.")
-
-        # Instructor scorecard selection GUI
-        instructors = data_handler.get_instructors(csv_path[0])
-        print("🖥️ Opening Instructor Selection GUI")
-        selected_scorecard_instructors = select_rows_gui.select_rows_gui(instructors)
-        print(f"  ✅ {len(selected_scorecard_instructors)} instructor(s) selected.")
-
-        # Run LLM IO
-        if (include_llm_insights):
-            print("🤖 Running LLM I/O")
-            pdf_json = utils.get_pdf_json(paths['parsed_pdf_dir'], data_vis_settings)
-            llm_io.run_llm(gguf_path=paths['gguf_path'],
-                        pdf_json= pdf_json,
-                        llm_dir= paths['llm_prompt_dir'],
-                        temp_dir= paths['temp_dir'])
-        
-        # Generate data visualizations
-        print("📈 Generating Data Visualizations")
-        data_vis.generate_data_visualization(
-            config, 
-            selected_scorecard_courses, 
-            selected_scorecard_instructors, 
-            csv_path[0], 
-            selected_history_courses)
-
-        # List of this tuple: (csv row, matching json pdf)
-        scorecards_to_generate = viable_scorecards_tuple[1]
-        # TODO: Assemble & Save Scorecard PDF
-        print("📝 Generating LaTeX")
-        # Iterate through the scorecards to generate one at a time
-        for scorecard_set in scorecards_to_generate:
-            scorecard_assembler.assemble_scorecard(
-                scorecard_set=scorecard_set, 
-                histogram_dir=paths['grade_histogram_dir'],
-                tex_output_path=paths['tex_dir'],
-                scorecard_output_path=paths['scorecard_dir'])
+        app.scorecard_selection()
+        app.history_selection()
+        app.instructor_selection()
+        app.gather_llm_insights()
+        app.generate_data_visualizations()
+        app.create_scorecards()
 
         # TODO: add cleanup function for temp in utils
     except Exception as e:
